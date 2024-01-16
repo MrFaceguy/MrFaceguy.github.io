32a017441ea1a70189ea87d4586ad85f1450442e
MrFaceguy <58958252+MrFaceguy@users.noreply.github.com>
2024-01-16 00:35:57 -0500

    New additions to homepage and new page itself

---
+++ happy.js
@@ -0,0 +1,11 @@
+function hideAndShow() {
+    // Hide the button
+    document.getElementById("hideButton").style.display = "none";
+
+    // Show the gif and audio
+    document.getElementById("gifContainer").style.display = "block";
+
+    // Play the audio
+    document.getElementById("audio").play();
+	audio.volume = .15;
+}
