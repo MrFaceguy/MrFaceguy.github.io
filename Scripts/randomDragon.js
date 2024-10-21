25f22061177e8b56f4b1e54fb0391de1f6814758
MrFaceguy <58958252+MrFaceguy@users.noreply.github.com>
2024-10-20 20:11:28 -0400

    added api item

---
+++ Scripts/randomDragon.js
@@ -0,0 +1,24 @@
+const dragonTypeList = "https://pokeapi.co/api/v2/type/dragon/";
+const dragonFinderTemplate = "https://pokeapi.co/api/v2/pokemon/"
+async function getPokemon(){
+    rnum = Math.floor(Math.random() * 107);
+    const pokemonChosen = await fetch(dragonTypeList)
+    pokemonChosen.json().then(data => {
+        let allDragons = data.pokemon;
+        let monsterName = allDragons[rnum].pokemon.name;
+        const outputHTML = document.getElementById("nameSection");
+        outputHTML.innerHTML = monsterName;
+        getImage(monsterName);
+    });
+}
+async function getImage(monsterName){
+    findingImg = await fetch(dragonFinderTemplate + monsterName)
+        findingImg.json().then(data => {
+            let specificDragon = data.sprites.front_default;
+            const findingImg = document.getElementById("imageSection");
+            findingImg.setAttribute("alt", monsterName + " (is unavailable)");
+            findingImg.setAttribute("src", specificDragon);
+        });
+}
+
+getPokemon();
